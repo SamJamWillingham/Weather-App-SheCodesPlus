1bdec93b60e89ad019196dca2dde822c716889e6
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-08-17 15:16:32 -0400

    wrote js for forecast with time, icon, and temp max/min

--- js-data-types.js
+++ js-data-types.js
@@ -1,4 +1,4 @@
-//converting C and F next to temperature
+//converting C and F next to temperature with vanilla javascript
 function convertToFahrenheit(event) {
   event.preventDefault();
   let fahrenheitTemperature = (celsuisTemperature * 9) / 5 + 32;
@@ -23,16 +23,8 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 let celsuisLink = document.querySelector("#celsuisLink");
 celsuisLink.addEventListener("click", convertToCelsuis);
 
-//Current time display
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+//Current time display with vanilla javascript
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 let months = [
   "January",
   "February",
@@ -58,7 +50,17 @@ let minutes = now.getMinutes();
 let currentDate = document.querySelector("#currentDate");
 currentDate.innerHTML = `${day}, ${month} ${date}, ${hour}:${minutes}`;
 
-//Search from form input
+//time display in forecast (API)
+function formatTime(timestamp) {
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+//Search from form input (API)
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#inlineFormInputName2");
@@ -67,6 +69,9 @@ function search(event) {
   let apiKey = "f8789029e0a5277fb2e5a66c29f35e2c";
   let apiUrl = `${apiEndpoint}q=${searchCity}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(localWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 //Geolocation API + localWeather function (display info from search or current location)
@@ -105,6 +110,9 @@ function showPosition(position) {
   let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(localWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function currentPosition(event) {
@@ -119,6 +127,30 @@ locationButton.addEventListener("click", currentPosition);
 let cityForm = document.querySelector("#city-form");
 cityForm.addEventListener("submit", search);
 
+//forecast (API)
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `<div class="col-2-auto">
+    <p>${formatTime(forecast.dt * 1000)}</p>
+    <img src= "http://openweathermap.org/img/wn/${
+      forecast.weather[0].icon
+    }@2x.png"
+    width=40px 
+    alt="sun" 
+    class="forecast-icon">
+    <br />
+    <p><strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(
+      forecast.main.temp_min
+    )}°
+    </p>
+</div>`;
+  }
+}
+//This function gives up-to-date weather info. for Charlotte, NC when the site first loads.
 function firstUpdate() {
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
   let apiKey = "f8789029e0a5277fb2e5a66c29f35e2c";
@@ -126,6 +158,9 @@ function firstUpdate() {
   let apiUrl = `${apiEndpoint}q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(localWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 firstUpdate();
